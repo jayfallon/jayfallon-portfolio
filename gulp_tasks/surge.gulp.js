9a29bef2de733f6fc3ff4e6e9526ee55e0c6c887
Jay Fallon <jayfallon@gmail.com>
2016-11-01 15:03:08 -0400

    made edits to content, removed extra files

--- gulp_tasks/surge.gulp.js
+++ gulp_tasks/surge.gulp.js
@@ -4,6 +4,6 @@ var surge = require('gulp-surge')
 gulp.task('deploy', [], function () {
   return surge({
     project: './dist',         // Path to your static build directory
-    domain: 'jayfallon.net'  // Your domain or Surge subdomain
+    //domain: 'jayfallon.net'  // Your domain or Surge subdomain
   })
 })
